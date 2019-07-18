081f482e29bedb78502000b83be6de6c08ae67ac
Devil <bhumijgupta@gmail.com>
2019-07-19 02:07:22 +0530

    Fixed broken unitest

--- test_link_checker.py
+++ test_link_checker.py
@@ -187,13 +187,12 @@ def test_get_scrapable_links():
 
 
 def test_exception_handler():
-    # TODO: Check requests-mock module for mocking requests
     links_list = ["http://www.google.com:81", "file://C:/Devil"]
     rs = (grequests.get(link, timeout=3) for link in links_list)
     response = grequests.map(
         rs, exception_handler=link_checker.exception_handler
     )
-    assert response == ["Connection Error", "Invalid Schema"]
+    assert response == ["Timeout Error", "Invalid Schema"]
 
 
 def test_map_links_file(reset_global):

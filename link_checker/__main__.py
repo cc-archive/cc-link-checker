985dafc59bca3b1d23deb4c9d06cdd249cfdadb8
Khanh Nguyen <khanhnguyen99hn@gmail.com>
2020-09-16 19:22:46 -0400

    format code

--- link_checker/__main__.py
+++ link_checker/__main__.py
@@ -242,7 +242,7 @@ def check_deeds(args):
         deed_base_url = create_base_link(args, filename, for_deeds=True)
         # Deeds template:
         # https://github.com/creativecommons/cc.engine/blob/master/cc/engine/templates/legalcode/standard_deed.html
-        
+
         # Scrapping the html found on the active site
         if deed_base_url:
             context = f"\n\nChecking: deed\nURL: {deed_base_url}"
